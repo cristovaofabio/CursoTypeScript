93d899dbb84aa7977096f46e489f2ce4bcd57544
Cristovao <cristovaofabio3@gmail.com>
2022-04-26 09:01:15 -0300

    super class

--- introduction/src/type_class/Person.ts
+++ introduction/src/type_class/Person.ts
@@ -6,14 +6,32 @@ export class Person {
         protected cpf: string,
     ) { }
 
+    getMessage(): string {
+        return 'Hello, I am a person';
+    }
+
 }
 
 export class Student extends Person {
-    //--------------//
+    getMessage(): string {
+        console.log('I am testing a super class');
+        return super.getMessage();
+    }
 }
 export class Teacher extends Person {
-    //--------------//
+    constructor(
+        name: string,
+        last_name: string,
+        age: number,
+        cpf: string,
+        public school_class: string
+    ) {
+        super(name, last_name, age, cpf);
+    }
 }
 
-const student = new Student('Mike', 'Tyson', 50, '000.000.000-00');
-console.log(student);
+const student = new Student('Mike', 'Tyson', 20, '000.000.000-00');
+const teacher = new Teacher('Travor', 'Brant', 50, '111.111.111-11', 'Geography')
+
+console.log(student.getMessage());
+console.log(teacher);
