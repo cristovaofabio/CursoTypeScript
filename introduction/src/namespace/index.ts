a9716e70bf2d45e5e152e9197c0b914bf48fba4b
Cristovao <cristovaofabio3@gmail.com>
2022-05-03 20:07:40 -0300

    import JavaScript file

--- introduction/src/namespace/index.ts
+++ introduction/src/namespace/index.ts
@@ -1,3 +1,10 @@
+/*
 /// <reference path="module/my_namespace.ts"/>
 
 console.log(myNewNamespace.PersonNamespace);
+*/
+
+import { sum } from './js/module';
+
+const result = sum(10, 30) as number;
+console.log(result);
